55b0d41154a85cefdead9e9f476c5cfc539fe615
Daniel Zabat <danielzabat01@gmail.noreply.github.com>
2025-01-17 22:21:39 +0800

    refactor: change the game logic to array methods

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
-const ticTactToe = (function () {
-  const gameBoard = [
-    "empty",
-    "empty",
-    "empty",
-    "empty",
-    "empty",
-    "empty",
-    "empty",
-    "empty",
-    "empty",
+const ticTacToe = (function () {
+  const gameBoard = Array(9).fill("empty");
+  const winningCombination = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
   ];
   const players = [];
-  const symbols = ["X", "O"];
   let currentPlayer;
 
-  const makePlayer = (name, symbol) => {
-    return { name, symbol };
-  };
+  const makePlayer = (name, symbol) => ({ name, symbol });
 
   const addPlayer = (name, symbol) => {
     const newPlayer = makePlayer(name, symbol);
@@ -30,64 +27,38 @@ const ticTactToe = (function () {
   };
 
   const switchPlayer = () => {
-    if (currentPlayer === players[0]) {
-      currentPlayer = players[1];
-    } else if (currentPlayer === players[1]) {
-      currentPlayer = players[0];
-    }
+    currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
   };
 
   const gameController = (index) => {
-    fillGameBoard(index, currentPlayer.symbol);
-    gameLogic();
-  };
-  const fillGameBoard = (index, symbol) => {
-    if (gameBoard[index] === "empty") {
-      gameBoard[index] = symbol;
+    if (gameBoard[index] !== "empty" || index > gameBoard.length || index < 0) {
+      console.log("invalid input");
+      return;
     }
-  };
-  const gameLogic = () => {
-    if (combination(0, 1, 2)) {
-      console.log(`The Winner is ${currentPlayer.name}`);
-    } else if (combination(3, 4, 5)) {
-      console.log(`The Winner is ${currentPlayer.name}`);
-    } else if (combination(6, 7, 8)) {
-      console.log(`The Winner is ${currentPlayer.name}`);
-    } else if (combination(0, 3, 6)) {
-      console.log(`The Winner is ${currentPlayer.name}`);
-    } else if (combination(1, 4, 7)) {
-      console.log(`The Winner is ${currentPlayer.name}`);
-    } else if (combination(2, 5, 8)) {
-      console.log(`The Winner is ${currentPlayer.name}`);
-    } else if (combination(0, 4, 8)) {
-      console.log(`The Winner is ${currentPlayer.name}`);
-    } else if (combination(2, 4, 6)) {
-      console.log(`The Winner is ${currentPlayer.name}`);
-    } else if (isGameOver()) {
-      console.log("Draw");
-    } else {
-      console.log("Not Finished");
+
+    fillGameBoard(index, currentPlayer.symbol);
+
+    if (checkWinner()) {
+      console.log(`The winner is ${currentPlayer.name}`);
       console.log(gameBoard);
-      console.log(currentPlayer.name);
+    } else if (isGameOver()) {
+      console.log(`It's a draw!`);
+      console.log(gameBoard);
+    } else {
+      console.log(`Not Finished`);
+      console.log(gameBoard);
       switchPlayer();
     }
   };
-
-  const combination = (num1, num2, num3) => {
-    if (
-      (gameBoard[num1] === symbols[0] &&
-        gameBoard[num2] === symbols[0] &&
-        gameBoard[num3] === symbols[0]) ||
-      (gameBoard[num1] === symbols[1] &&
-        gameBoard[num2] === symbols[1] &&
-        gameBoard[num3] === symbols[1])
-    ) {
-      return true;
-    } else {
-      return false;
-    }
+  const fillGameBoard = (index, symbol) => {
+    gameBoard[index] = symbol;
   };
 
+  const checkWinner = () => {
+    return winningCombination.some((combination) =>
+      combination.every((index) => gameBoard[index] === currentPlayer.symbol)
+    );
+  };
   const isGameOver = () => gameBoard.every((el) => el !== "empty");
 
   return { addPlayer, gameController, players, gameBoard, currentPlayer };
