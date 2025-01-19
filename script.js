048097bdef9490a07d5f1cd367e0e7b604a727a1
Daniel Zabat <danielzabat01@gmail.noreply.github.com>
2025-01-19 21:04:07 +0800

    feat: add ui for the game

--- script.js
+++ script.js
@@ -11,7 +11,9 @@ const gameBoard = (function () {
 
   const getBoard = () => board;
 
-  return { isGameOver, fill, getBoard };
+  const resetBoard = () => board.fill("empty");
+
+  return { isGameOver, fill, getBoard, resetBoard };
 })();
 
 const player = (function () {
@@ -31,21 +33,31 @@ const gameController = (function () {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  const players = [];
+  const players = [
+    { name: "Player 1", symbol: "X", score: 0 },
+    { name: "Player 2", symbol: "O", score: 0 },
+  ];
   let currentPlayer;
+  currentPlayer = players[0];
+  let isFinished = false;
 
-  const addPlayer = (name, symbol) => {
-    const newPlayer = player.makePlayer(name, symbol);
-    if (players.length < 2) {
-      players.push(newPlayer);
-    }
+  // const addPlayer = (name, symbol) => {
+  //   const newPlayer = player.makePlayer(name, symbol);
+  //   if (players.length < 2) {
+  //     players.push(newPlayer);
+  //   }
 
-    if (players.length === 1) {
-      currentPlayer = players[0];
-    }
-  };
+  //   if (players.length === 1) {
+  //     currentPlayer = players[0];
+  //   }
+  // };
 
   const board = gameBoard.getBoard();
+
+  const resetGame = () => {
+    gameBoard.resetBoard();
+    isFinished = false;
+  };
 
   const checkWinner = () => {
     return winningCombination.some((combination) =>
@@ -57,26 +69,68 @@ const gameController = (function () {
     currentPlayer = currentPlayer === players[0] ? players[1] : players[0];
   };
 
-  const controller = (index) => {
+  const addSymbols = (event) => {
+    event.target.textContent = currentPlayer.symbol;
+  };
+
+  const controller = (index, event) => {
     if (board[index] !== "empty" || index > board.length || index < 0) {
       console.log("invalid input");
       return;
     }
 
-    gameBoard.fill(index, currentPlayer.symbol);
+    if (!isFinished) {
+      gameBoard.fill(index, currentPlayer.symbol);
+      addSymbols(event);
+    }
 
     if (checkWinner()) {
       console.log(`The winner is ${currentPlayer.name}`);
-      console.log(board);
+      isFinished = true;
     } else if (gameBoard.isGameOver()) {
       console.log(`It's a draw!`);
-      console.log(board);
+      isFinished = true;
     } else {
       console.log(`Not Finished`);
-      console.log(board);
       switchPlayer();
     }
   };
 
-  return { controller, addPlayer };
+  return { controller, resetGame };
 })();
+
+const displayGameBoard = () => {
+  const ticTacToeBoard = document.getElementById("tic-tac-toe-board");
+  ticTacToeBoard.innerHTML = "";
+  const fragment = document.createDocumentFragment();
+  const board = gameBoard.getBoard();
+
+  for (let i = 0; i < board.length; i++) {
+    const box = document.createElement("button");
+    box.classList.add("spots");
+    box.dataset.index = i;
+    fragment.append(box);
+  }
+
+  ticTacToeBoard.appendChild(fragment);
+};
+
+const eventHandler = (event) => {
+  if (event.target.classList.contains("spots")) {
+    const indexTarget = event.target.dataset.index;
+
+    const indexText = document.querySelector(".index-number");
+    indexText.textContent = event.target.dataset.index;
+
+    gameController.controller(indexTarget, event);
+  }
+
+  if (event.target.id === "reset") {
+    displayGameBoard();
+    gameController.resetGame();
+  }
+};
+
+document.addEventListener("click", (event) => eventHandler(event));
+
+displayGameBoard();
